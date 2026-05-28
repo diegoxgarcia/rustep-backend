b2fd3d18e7047eec36f83327714b59254033814c
diegoxgarcia <diegoxgarcia@gmail.com>
2026-05-27 23:52:29 -0300

    fix: remove duplicate index: true on userId in StepsLog

--- models/StepsLog.js
+++ models/StepsLog.js
@@ -17,7 +17,6 @@ const stepsLogSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
-    index: true
   },
   stepsCount: {
     type: Number,
